0a9224007e96b8067c36ac39c0ad33fb219fe7a2
Ingmar Steen <iksteen@gmail.com>
2014-03-24 16:36:14 +0100

    Client no longer adds itself as an event argument.
    Instead, a dictionary with server info is added. This is to make the event data JSON encodable.

--- twisted/plugins/automatron_registration.py
+++ twisted/plugins/automatron_registration.py
@@ -39,65 +39,65 @@ class AutomatronRegistration(object):
             message
         )
 
-    def on_command(self, client, user, command, args):
+    def on_command(self, server, user, command, args):
         config = self.command_map.get(command)
         if config is None:
             return
 
         if not config[1] <= len(args) <= config[2]:
-            self._msg(client.server, user, 'Invalid syntax. Use: %s %s' % (command, config[0]))
+            self._msg(server['server'], user, 'Invalid syntax. Use: %s %s' % (command, config[0]))
         else:
-            getattr(self, '_on_command_%s' % command)(client, user, *args)
+            getattr(self, '_on_command_%s' % command)(server, user, *args)
         return STOP
 
     @defer.inlineCallbacks
-    def _on_command_identify(self, client, user, channel=None):
-        username, username_relevance = yield self.controller.config.get_username_by_hostmask(client.server, user)
+    def _on_command_identify(self, server, user, channel=None):
+        username, username_rel = yield self.controller.config.get_username_by_hostmask(server['server'], user)
         if username is not None:
-            if username_relevance == 0:
+            if username_rel == 0:
                 identity = 'You are globally known as %s' % username
             else:
                 identity = 'You are known as %s' % username
 
-            role, role_relevance = yield self.controller.config.get_role_by_username(client.server, channel, username)
-            if role_relevance is not None and role_relevance < username_relevance:
-                role = role_relevance = None
+            role, role_rel = yield self.controller.config.get_role_by_username(server['server'], channel, username)
+            if role_rel is not None and role_rel < username_rel:
+                role = role_rel = None
 
-            if role_relevance is None:
-                self._msg(client.server, user, identity)
-            elif role_relevance in (2, 3):
-                self._msg(client.server, user, '%s and your role in %s is %s' % (identity, channel, role))
+            if role_rel is None:
+                self._msg(server['server'], user, identity)
+            elif role_rel in (2, 3):
+                self._msg(server['server'], user, '%s and your role in %s is %s' % (identity, channel, role))
             else:
-                self._msg(client.server, user, '%s and your role is %s' % (identity, role))
+                self._msg(server['server'], user, '%s and your role is %s' % (identity, role))
         else:
-            self._msg(client.server, user, 'I don\'t know you...')
+            self._msg(server['server'], user, 'I don\'t know you...')
 
     @defer.inlineCallbacks
-    def _on_command_register(self, client, user, password, email):
+    def _on_command_register(self, server, user, password, email):
         nickname = parse_user(user)[0]
         pw_hash = pwd_context.encrypt(password)
 
-        config = yield self.controller.config.get_plugin_section(self, client.server, None)
-        mail_config = yield self.controller.config.get_section('mail', client.server, None)
+        config = yield self.controller.config.get_plugin_section(self, server['server'], None)
+        mail_config = yield self.controller.config.get_section('mail', server['server'], None)
 
         if not config.get('registration') == 'true' or not mail_config.get('from'):
-            self._msg(client.server, user, 'User registration is disabled.')
+            self._msg(server['server'], user, 'User registration is disabled.')
             return
 
         _, user_email_rel = yield self.controller.config.get_value(
             'user.email',
-            client.server,
+            server['server'],
             None,
             nickname
         )
         _, verify_rel = yield self.controller.config.get_value(
             'user.verify',
-            client.server,
+            server['server'],
             None,
             nickname
         )
         if user_email_rel is not None or verify_rel is not None:
-            self._msg(client.server, user, 'User %s already exists.' % nickname)
+            self._msg(server['server'], user, 'User %s already exists.' % nickname)
             return
 
         verification_code = ''
@@ -112,7 +112,7 @@ class AutomatronRegistration(object):
 
         self.controller.config.update_value(
             'user.verify',
-            client.server,
+            server['server'],
             None,
             nickname,
             json.dumps(verify_data),
@@ -126,57 +126,57 @@ following command on IRC:
 
 Kind regards,
 %(mynickname)s automailer
-""" % {'nickname': nickname, 'mynickname': client.nickname, 'code': verification_code})
-        msg['Subject'] = '%s account registration' % client.nickname
+""" % {'nickname': nickname, 'mynickname': server['nickname'], 'code': verification_code})
+        msg['Subject'] = '%s account registration' % server['nickname']
         msg['From'] = mail_config['from']
         msg['To'] = email
         sendmail(mail_config.get('mailserver', 'localhost'), mail_config['from'], [email], msg.as_string())
 
-        self._msg(client.server, user, 'An email with verification instructions has been sent.')
+        self._msg(server['server'], user, 'An email with verification instructions has been sent.')
 
     @defer.inlineCallbacks
-    def _on_command_verify(self, client, user, verification_code):
+    def _on_command_verify(self, server, user, verification_code):
         nickname = parse_user(user)[0]
 
-        verify_data, _ = yield self.controller.config.get_value('user.verify', client.server, None, nickname)
+        verify_data, _ = yield self.controller.config.get_value('user.verify', server['server'], None, nickname)
         if not verify_data:
-            self._msg(client.server, user, 'Please register first.')
+            self._msg(server['server'], user, 'Please register first.')
             return
 
         try:
             verify_data = json.loads(verify_data)
         except Exception as e:
             log.err(e, 'Could not decode verification data')
-            self._msg(client.server, user, 'Something went terribly wrong.')
+            self._msg(server['server'], user, 'Something went terribly wrong.')
             return
 
         if verify_data['code'] != verification_code:
-            self._msg(client.server, user, 'Incorrect verification code. Please double check.')
+            self._msg(server['server'], user, 'Incorrect verification code. Please double check.')
             return
 
         self.controller.config.update_value(
             'user.email',
-            client.server,
+            server['server'],
             None,
             nickname,
             verify_data['email'].encode('utf-8'),
         )
         self.controller.config.update_value(
             'user.password',
-            client.server,
+            server['server'],
             None,
             nickname,
             verify_data['password'].encode('utf-8'),
         )
 
-        default_role, _ = yield self.controller.config.get_plugin_value(self, client.server, None, 'default_role')
+        default_role, _ = yield self.controller.config.get_plugin_value(self, server['server'], None, 'default_role')
         if default_role:
             self.controller.config.update_value(
                 'user.role',
-                client.server,
+                server['server'],
                 None,
                 nickname,
                 default_role
             )
 
-        self._msg(client.server, user, 'Registration completed.')
+        self._msg(server['server'], user, 'Registration completed.')
