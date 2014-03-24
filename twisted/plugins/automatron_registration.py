287eb38750fc24398fbb628a6259941d3e6c1386
Ingmar Steen <iksteen@gmail.com>
2014-03-24 10:42:56 +0100

    Move controller to automatron.controller.
    This is the first phase in separating the controller from the plugin backend. There have been changes in all plugins and they'll change again in the future.

--- twisted/plugins/automatron_registration.py
+++ twisted/plugins/automatron_registration.py
@@ -5,8 +5,8 @@ from email.mime.text import MIMEText
 from twisted.internet import defer
 from twisted.mail.smtp import sendmail
 from zope.interface import implements, classProvides
-from automatron.command import IAutomatronCommandHandler
-from automatron.plugin import IAutomatronPluginFactory, STOP
+from automatron.controller.command import IAutomatronCommandHandler
+from automatron.controller.plugin import IAutomatronPluginFactory, STOP
 from passlib.apps import custom_app_context as pwd_context
 import json
 
